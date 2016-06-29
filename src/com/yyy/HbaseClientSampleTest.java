5d2f23800061379a8d01b06c2104b332537e09b4
waixiaoyu <yaoyu_best@163.com>
2016-06-29 12:09:36 +0800

    first commit

---
+++ src/com/yyy/HbaseClientSampleTest.java
@@ -0,0 +1,25 @@
+package com.yyy;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class HbaseClientSampleTest {
+	
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testInsertData() {
+		HbaseClientSample.insertData("MyTable");  
+	}
+
+}
